c96705b44b0b08f46863a5aa93ce983d347ebda9
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-10-07 22:04:42 +0200

    change css styles of the form component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import Input from './Input';
@@ -8,19 +9,20 @@ const useStyles = makeStyles(() => ({
     root: {
         display: 'flex',
         flexGrow: 1,
-        width: "50%",
-        margin: "0 auto"
+        width: '100%',
+        margin: "0 auto",
     },
 }))
 
 export const Form = (props) => {
-    const classes = useStyles();
     const [inputValue, setInputValue] = useState(null);
+
+    const classes = useStyles();
     const [buttonDisabled, setButtonDisabled] = useState(true);
     return (
-        <div className={classes.root}>
+        <form noValidate autoComplete="off" onSubmit={() => {}} className={classes.root}>
             <Input setInputValue={setInputValue} setButtonDisabled={setButtonDisabled}/>
             <Button disabled={buttonDisabled} variant="contained" type='submit' color="primary">{ strings.FORM_BUTTON_TEXT }</Button>
-        </div>
+        </form>
     )
 }
