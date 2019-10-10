6e38b37fdd048da4a3ac08da999d76155a8c436d
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-10-10 19:25:39 +0200

    add better error handling for fetching cities

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,10 +3,13 @@ import { fetchCities } from '../../utlis/fetchUtils';
 import { Form } from '../form/Form';
 import { DataList } from '../dataList/DataList';
 import './App.css';
+import { CustomSnackBar } from '../CustomSnackBar/CustomSnackBar';
 
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
   async function handleSubmit(inputValue, e) {
     setIsLoading(true);
@@ -15,10 +18,15 @@ function App() {
     if(cities) {
       setCities(cities);
     }
+    else {
+      setMessage("Connection error!")
+      setOpen(true);
+    }
     setIsLoading(false);
   }
     return (
       <div className="App">
+          <CustomSnackBar open={open} setOpen={setOpen} message={message} />
           <Form handleSubmit={handleSubmit}/>
           <DataList cities={cities} isLoading={isLoading} />
       </div>
